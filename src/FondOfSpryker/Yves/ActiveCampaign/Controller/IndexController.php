e763b49934ef899788f20cb15bb61aea06b44f51
Pascal Fischer <pascal.fischer@fondof.de>
2018-03-23 11:55:04 +0100

    Fixed an issue while redirecting after submit Add multi language route

--- src/FondOfSpryker/Yves/ActiveCampaign/Controller/IndexController.php
+++ src/FondOfSpryker/Yves/ActiveCampaign/Controller/IndexController.php
@@ -3,7 +3,9 @@
 namespace FondOfSpryker\Yves\ActiveCampaign\Controller;
 
 use Doctrine\Common\Annotations\AnnotationRegistry;
+use FondOfSpryker\Yves\ActiveCampaign\Plugin\Provider\ActiveCampaignControllerProvider;
 use Generated\Shared\Transfer\ActiveCampaignRequestTransfer;
+use Pyz\Yves\Application\Plugin\Provider\ApplicationControllerProvider;
 use Spryker\Yves\Kernel\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -18,7 +20,7 @@ class IndexController extends AbstractController
      *
      * @return array
      */
-    public function subscribeAction(Request $request)
+    public function formAction(Request $request)
     {
         AnnotationRegistry::registerLoader('class_exists');
 
@@ -51,11 +53,54 @@ class IndexController extends AbstractController
     }
 
     /**
+     * we need to request the submit on this function because we cant
+     * redirect an form rendering view.
+     *
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     *
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function submitAction(Request $request)
+    {
+        $activeCampaignSubscriptionForm = $this
+            ->getFactory()
+            ->getActiveCampaignSubscriptionForm();
+
+        $activeCampaignSubscriptionForm->handleRequest($request);
+
+        if ($activeCampaignSubscriptionForm->isValid()) {
+            /** @var \Generated\Shared\Transfer\ActiveCampaignRequestTransfer $transfer */
+            $transfer = new ActiveCampaignRequestTransfer();
+            $transfer
+                ->setEmail($activeCampaignSubscriptionForm->get('email')->getData())
+                ->setLocale($this->getLocale());
+
+            $response = $this->getClient()->subscribe($transfer);
+
+            return $this->redirectResponseInternal(ActiveCampaignControllerProvider::ROUTE_ACTIVECAMPAIGN_SUBSCRIBE, [
+                'newsletter' => 'newsletter',
+            ]);
+        } else {
+            return $this->redirectResponseInternal(ApplicationControllerProvider::ROUTE_HOME);
+        }
+    }
+
+    /**
      * @param \Symfony\Component\HttpFoundation\Request $request
      *
      * @return array
      */
     public function subscribeConfirmationAction(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     *
+     * @return array
+     */
+    public function subscribeAction(Request $request)
     {
         return [];
     }
--- src/FondOfSpryker/Yves/ActiveCampaign/Plugin/Provider/ActiveCampaignControllerProvider.php
+++ src/FondOfSpryker/Yves/ActiveCampaign/Plugin/Provider/ActiveCampaignControllerProvider.php
@@ -7,8 +7,14 @@ use Silex\Application;
 class ActiveCampaignControllerProvider extends AbstractYvesControllerProvider
 {
     const ROUTE_ACTIVECAMPAIGN_FOOTER = 'ROUTE_ACTIVECAMPAIGN_FOOTER';
+    const ROUTE_ACTIVECAMPAIGN_SUBMIT = 'ROUTE_ACTIVECAMPAIGN_SUBMIT';
     const ROUTE_ACTIVECAMPAIGN_SUBSCRIBE = 'ROUTE_ACTIVECAMPAIGN_SUBSCRIBE';
     const ROUTE_ACTIVECAMPAIGN_SUBSCRIBE_CONFIRM = 'ROUTE_ACTIVECAMPAIGN_SUBSCRIBE_CONFIRM';
+
+    const URL_NEWSLETTER_CONFIRMED_DE = 'anmeldebestaetigung';
+    const URL_NEWSLETTER_CONFIRMED_EN = 'subscribe-confirmation';
+    const URL_NEWSLETTER_SUBSCRIBE_DE = 'anmeldung';
+    const URL_NEWSLETTER_SUBSCRIBE_EN = 'subscribe';
 
     /**
      * @param \Silex\Application $app
@@ -19,27 +25,46 @@ class ActiveCampaignControllerProvider extends AbstractYvesControllerProvider
     {
         $allowedLocalesPattern = $this->getAllowedLocalesPattern();
 
-        $this->createController(
-            '{locale}/newsletter/subscribe/',
-            static::ROUTE_ACTIVECAMPAIGN_FOOTER,
-            'ActiveCampaign',
-            'Index',
-            'subscribe'
-        )->method('GET|POST');
+        $subscribe = ($app->offsetGet('locale') === 'de_DE')
+            ? self::URL_NEWSLETTER_SUBSCRIBE_DE
+            : self::URL_NEWSLETTER_SUBSCRIBE_EN;
+
+        $confirmation = ($app->offsetGet('locale') === 'de_DE')
+            ? self::URL_NEWSLETTER_CONFIRMED_DE
+            : self::URL_NEWSLETTER_CONFIRMED_EN;
 
         $this->createController(
-            '{newsletter}/subscribe/',
-            static::ROUTE_ACTIVECAMPAIGN_SUBSCRIBE,
+            '{newsletter}/submit/',
+            static::ROUTE_ACTIVECAMPAIGN_SUBMIT,
             'ActiveCampaign',
             'Index',
-            'subscribe'
+            'submit'
         )
             ->method('GET|POST')
             ->assert('newsletter', $allowedLocalesPattern . 'newsletter|newsletter');
         ;
 
         $this->createController(
-            '/newsletter/subscribe-confirmation/',
+            '{locale}/newsletter/form/',
+            static::ROUTE_ACTIVECAMPAIGN_FOOTER,
+            'ActiveCampaign',
+            'Index',
+            'form'
+        )->method('GET|POST');
+
+        $this->createController(
+            '{newsletter}/' . $subscribe . '/',
+            static::ROUTE_ACTIVECAMPAIGN_SUBSCRIBE,
+            'ActiveCampaign',
+            'Index',
+            'subscribe'
+        )
+            ->method('GET')
+            ->assert('newsletter', $allowedLocalesPattern . 'newsletter|newsletter');
+        ;
+
+        $this->createController(
+            '{newsletter}/' . $confirmation . '/',
             static::ROUTE_ACTIVECAMPAIGN_SUBSCRIBE_CONFIRM,
             'ActiveCampaign',
             'Index',
